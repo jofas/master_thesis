799ccac4577563abdc4221b523547340e9dfd48e
jofas <jonas@fc-web.de>
2020-07-03 16:55:00 +0200

    further tests to ensure correct usage of convolutions

---
+++ SpiDNN/test/test_conv.py
@@ -0,0 +1,92 @@
+from spiDNN.layers import Conv1D, Input
+
+import tensorflow as tf
+
+import numpy as np
+
+
+#column -> one filter
+weights = np.array([[[0.0, 1.0, 2.0],
+                     [0.1, 1.1, 2.1],
+                     [0.2, 1.2, 2.2]],
+                    [[0.3, 1.3, 2.3],
+                     [0.4, 1.4, 2.4],
+                     [0.5, 1.5, 2.5]],
+                    [[0.6, 1.6, 2.6],
+                     [0.7, 1.7, 2.7],
+                     [0.8, 1.8, 2.8]]], dtype=np.float32)
+
+biases = np.array([0.9, 1.9, 2.9], dtype=np.float32)
+
+kernel_size = 3
+n_channels = 3
+n_filters = 3
+
+
+def test_conv_flatten():
+    assert (weights[:,:,0] == np.array([[0.0, 0.1, 0.2],
+                                        [0.3, 0.4, 0.5],
+                                        [0.6, 0.7, 0.8]],
+                                       dtype=np.float32)).all()
+
+    conv1d = Conv1D((kernel_size,), "identity")
+    conv1d.n_filters = n_filters
+
+    input = Input(2)
+    input.n_filters = n_channels
+
+    weights_, biases_ = conv1d.generate_weights(input)
+
+    assert weights.shape == weights_.shape
+    assert biases.shape == biases_.shape
+
+    flattened_weights = np.empty((
+        kernel_size * n_channels + 1, n_filters))
+
+    for i in range(0, n_filters):
+        filter = weights[:,:,i]
+        filter = np.append(filter.flatten(), biases[i])
+        flattened_weights[:, i] = filter
+
+    flattened_weights = flattened_weights.flatten(order="F")
+
+    flattened_weights = flattened_weights.reshape(
+        kernel_size * n_channels + 1, n_filters, order="F")
+
+    extracted_weights = np.empty((kernel_size, n_channels, n_filters))
+    extracted_biases = np.empty((n_filters,))
+
+    for i in range(0, n_filters):
+        extracted_weights[:,:,i] = flattened_weights[:-1,i].reshape(
+            kernel_size, n_channels)
+        extracted_biases[i] = flattened_weights[-1,i]
+
+    assert (extracted_biases == biases).all()
+    assert (extracted_weights == weights).all()
+
+
+def test_convolution():
+    input_shape = (1, kernel_size, n_channels)
+
+    # rows are channels, columns are the input vectors
+    input = np.array([[[1., 0., 0.],
+                       [0., 1., 0.],
+                       [0., 0., 1.]]], dtype=np.float32)
+
+    assert input.shape == input_shape
+
+    layer = tf.keras.layers.Conv1D(
+        n_filters, kernel_size, input_shape=input_shape[1:])
+    layer(input)
+    layer.set_weights([weights, biases])
+    y = layer(input).numpy()
+
+    assert (y[0,0,:] == np.array([1.2 + biases[0],
+                                  4.2 + biases[1],
+                                  7.2 + biases[2]], dtype=np.float32)).all()
+
+
+if __name__ == "__main__":
+    test_conv_flatten()
+    test_convolution()
+    print("SUCCESS.")
