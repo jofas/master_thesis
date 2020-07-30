63ad02a50f559fc042475cd0a4920ebce13dc0b6
jofas <jonas@fc-web.de>
2020-07-30 13:43:41 +0200

    removed perceptron.h

--- SpiDNN/src/perceptron.c
+++ SpiDNN/src/perceptron.c
@@ -1,4 +1,4 @@
-#include "perceptron.h"
+#include "spiDNN.h"
 
 #ifdef trainable
 #include "trainable.h"
@@ -7,6 +7,84 @@
 #ifdef softmax
 #include "softmax.h"
 #endif
+
+#define BIAS weights[n_weights - 1]
+#define N_POTENTIALS n_weights - 1
+
+
+//! definitions of each element in the base_params region
+typedef struct base_params_region {
+  uint32_t forward_key;
+  uint32_t min_pre_key;
+  uint32_t timer_offset;
+  uint32_t n_weights;
+  uint32_t activation_function_id;
+} base_params_region_t;
+
+
+/* global variables */
+
+uint forward_key;
+
+uint n_weights;
+
+uint activation_function_id;
+
+float *weights;
+
+float potential;
+
+float *weights_sdram;
+base_params_region_t *base_params_sdram;
+
+
+/* functions */
+
+void generate_potential(void) {
+  for (uint i = 0; i < n_potentials; i++) {
+    potential += potentials[i] * weights[i];
+  }
+  potential += BIAS;
+}
+
+void activate(void) {
+  generate_potential();
+
+  switch (activation_function_id) {
+    case IDENTITY:
+      break;
+
+    case RELU:
+      potential = potential > .0 ? potential : .0;
+      break;
+
+    case SIGMOID:
+      potential = 1. / (1. + exp(-potential));
+      break;
+
+    case TANH:
+      potential = tanh(potential);
+      break;
+
+    case SOFTMAX:
+      potential = exp(potential);
+      break;
+
+    default:
+      log_error("Unknown activation function %d - exiting!",
+        activation_function_id);
+      rt_error(RTE_SWERR);
+  }
+}
+
+void weights_init(void) {
+  weights_sdram = data_specification_get_region(WEIGHTS, data_spec_meta);
+
+  weights = (float *)malloc(sizeof(float) * n_weights);
+
+  sark_mem_cpy((void *)weights, (void *)weights_sdram,
+    sizeof(float) * n_weights);
+}
 
 void receive(uint key, float payload) {
   //log_info("received potential from %d: %f", key, payload);
@@ -100,4 +178,21 @@ void c_main(void) {
 
   log_info("\nStarting simulation\n");
   simulation_run();
+}
+
+
+/* function which has to be implemented by a machine vertex including
+ * spiDNN.h */
+void __init_base_params(
+    uint32_t *timer_offset, uint *n_potentials, uint *min_pre_key)
+{
+  base_params_sdram = data_specification_get_region(BASE_PARAMS, data_spec_meta);
+
+  forward_key = base_params_sdram->forward_key;
+  n_weights = base_params_sdram->n_weights;
+  activation_function_id = base_params_sdram->activation_function_id;
+
+  *timer_offset = base_params_sdram->timer_offset;
+  *n_potentials = n_weights - 1;
+  *min_pre_key = base_params_sdram->min_pre_key;
 }
