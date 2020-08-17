15952491c260232266a0fd821d4c1947aad72374
jofas <jonas@fc-web.de>
2020-08-17 15:08:12 +0200

    changed test script

--- SpiDNN/test/test_inference.py
+++ SpiDNN/test/test_inference.py
@@ -3,8 +3,7 @@ from spiDNN.layers import Input, Dense, Conv1D
 
 import numpy as np
 
-from keras.layers import Dense as KDense, Conv1D as KConv1D, Flatten
-from keras.models import Sequential
+import tensorflow as tf
 
 
 N = 224
@@ -15,19 +14,20 @@ kernel_size = 3
 def test_inference():
     X = np.random.rand(500, N)
 
-    kmodel = Sequential()
-    kmodel.add(KDense(100, activation="relu", input_shape=(N,)))
-    kmodel.add(KDense(100, activation="softmax"))
-    kmodel.add(KDense(300, activation="tanh"))
-    kmodel.add(KDense(50, activation="sigmoid"))
-    kmodel.add(KDense(25, activation="softmax"))
+    kmodel = tf.keras.models.Sequential()
+    kmodel.add(tf.keras.layers.Dense(100, activation="relu", input_shape=(N,)))
+    kmodel.add(tf.keras.layers.Dense(100, activation="softmax"))
+    kmodel.add(tf.keras.layers.Dense(300, activation="tanh"))
+    kmodel.add(tf.keras.layers.Dense(50, activation="sigmoid"))
+    kmodel.add(tf.keras.layers.Dense(25, activation="softmax"))
 
-    model = Model().add(Input(N)) \
-                   .add(Dense(100, activation="relu")) \
-                   .add(Dense(100, activation="softmax")) \
-                   .add(Dense(300, activation="tanh")) \
-                   .add(Dense(50, activation="sigmoid")) \
-                   .add(Dense(25, activation="softmax"))
+    model = Model()
+    model.add(Input(N))
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(100, activation="softmax"))
+    model.add(Dense(300, activation="tanh"))
+    model.add(Dense(50, activation="sigmoid"))
+    model.add(Dense(25, activation="softmax"))
 
     model.set_weights(kmodel.get_weights())
 
@@ -42,21 +42,31 @@ def test_inference_conv1d():
     input_shape = (N, n_channels)
     X = np.random.rand(500, *input_shape)
 
-    kmodel = Sequential()
-    kmodel.add(KConv1D(1, 7, padding="same", input_shape=input_shape, strides=2, activation="relu"))
-    kmodel.add(KConv1D(5, kernel_size * 4, padding="same", strides=3, activation="tanh"))
-    kmodel.add(KConv1D(16, kernel_size, padding="same", strides=2, activation="sigmoid"))
-    kmodel.add(KConv1D(16, kernel_size + 3, padding="same", strides=5, activation="softmax"))
-    kmodel.add(KConv1D(5, kernel_size + 1, strides=3))
-    kmodel.add(Flatten())
-    kmodel.add(KDense(1))
+    kmodel = tf.keras.models.Sequential()
+    kmodel.add(tf.keras.layers.Conv1D(
+        1, 7, padding="same", input_shape=input_shape, strides=2,
+        activation="relu"))
+    kmodel.add(tf.keras.layers.Conv1D(
+        5, kernel_size * 4, padding="same", strides=3, activation="tanh"))
+    kmodel.add(tf.keras.layers.Conv1D(
+        16, kernel_size, padding="same", strides=2, activation="sigmoid"))
+    kmodel.add(tf.keras.layers.Conv1D(
+        16, kernel_size + 3, padding="same", strides=5, activation="softmax"))
+    kmodel.add(tf.keras.layers.Conv1D(
+        5, kernel_size + 1, strides=3))
+    kmodel.add(tf.keras.layers.Flatten())
+    kmodel.add(tf.keras.layers.Dense(1))
 
     model = Model()
     model.add(Input(*input_shape))
     model.add(Conv1D(1, (7,), padding="same", stride=2, activation="relu"))
-    model.add(Conv1D(5, (kernel_size * 4,), padding="same", stride=3, activation="tanh"))
-    model.add(Conv1D(16, (kernel_size,), padding="same", stride=2, activation="sigmoid"))
-    model.add(Conv1D(16, (kernel_size + 3,), padding="same", stride=5, activation="softmax"))
+    model.add(Conv1D(
+        5, (kernel_size * 4,), padding="same", stride=3, activation="tanh"))
+    model.add(Conv1D(
+        16, (kernel_size,), padding="same", stride=2, activation="sigmoid"))
+    model.add(Conv1D(
+        16, (kernel_size + 3,), padding="same", stride=5,
+        activation="softmax"))
     model.add(Conv1D(5, (kernel_size + 1,), stride=3))
     model.add(Dense(1))
 
@@ -70,6 +80,6 @@ def test_inference_conv1d():
 
 
 if __name__ == "__main__":
-    # test_inference()
+    #test_inference()
     test_inference_conv1d()
     print("SUCCESS.")
